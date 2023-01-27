24e9c7e9bb566caba20154543cb121c74d271818
Mark Entingh <entingh@gmail.com>
2023-01-27 16:16:34 -0600

    updated selenium & Saber.Core (1.0.8)

---
+++ InternalApis.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Saber.Core;
+using Saber.Vendor;
+
+namespace Saber.Vendors.Screenshot
+{
+    public class InternalApis : IVendorInteralApis
+    {
+        public List<InternalApi> Apis { get; set; } = new List<InternalApi>()
+        {
+            new InternalApi(){Key = "Take-Screenshot", Method = Delegates.TakeScreenshot }
+        };
+
+        public static string TakeScreenshot(IRequest request, Dictionary<string, object> data)
+        {
+            var url = data.ContainsKey("url") ? (string)data["url"] : "";
+            var width = data.ContainsKey("width") ? (int)data["width"] : 1920;
+            var height = data.ContainsKey("height") ? (int)data["height"] : 1080;
+            return Screenshot.Take(request, url, width, height);
+        }
+
+        public static class Delegates
+        {
+            public static Func<IRequest, Dictionary<string, object>, string> TakeScreenshot { get; set; } = InternalApis.TakeScreenshot;
+        }
+    }
+}
--- Screenshot.cs
+++ Screenshot.cs
@@ -1,23 +1,47 @@
 ﻿using System;
+using System.IO;
+using Saber.Core;
 using Saber.Vendor;
 using Saber.Core.Extensions.Strings;
 using OpenQA.Selenium;
 
 namespace Saber.Vendors.Screenshot
 {
-    public class Screenshot: Core.Service, IVendorService
+    public class Screenshot: Service, IVendorService
     {
-        private string OutputPath = "/Content/screenshots/";
+        private static string OutputPath = "/Content/screenshots/";
+
         public string Take (string url, int width, int height)
         {
+            return Take(this, url, width, height);
+        }
+
+        public static string Take(IRequest request, string url, int width, int height)
+        {
             var id = DateTime.Now.ToString("yyyyMMdd-hhmm-") + url.ReplaceOnlyAlphaNumeric().Replace("/", "-").Replace(" ", "-");
-            WebDriver.Chrome.Navigate().GoToUrl(App.Host + "/" + url);
-            WebDriver.Chrome.Manage().Window.Size = new System.Drawing.Size(width, height);
-            IJavaScriptExecutor js = (IJavaScriptExecutor)WebDriver.Chrome;
-            js.ExecuteScript(@"document.body.style.overflow = 'hidden';");
-            var screenshot = ((ITakesScreenshot)WebDriver.Chrome).GetScreenshot();
-            screenshot.SaveAsFile(App.MapPath(OutputPath + id + ".jpg"), ScreenshotImageFormat.Jpeg);
-            return OutputPath + id + ".jpg";
+            if (id.Length > 28)
+            {
+                id = id.Substring(0, 28);
+            }
+            try
+            {
+                WebDriver.Chrome.Navigate().GoToUrl(App.Host + "/" + url);
+                WebDriver.Chrome.Manage().Window.Size = new System.Drawing.Size(width, height);
+                IJavaScriptExecutor js = WebDriver.Chrome;
+                if(js != null)
+                {
+                    js.ExecuteScript(@"document.body.style.overflow = 'hidden';");
+                }
+                var screenshot = ((ITakesScreenshot)WebDriver.Chrome).GetScreenshot();
+                screenshot.SaveAsFile(App.MapPath(OutputPath + id + ".png"), ScreenshotImageFormat.Png);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, request, "api/Screenshot/Take");
+                throw new Exception("Error saving screenshot", ex);
+                
+            }
+            return id + ".jpg";
         }
     }
 }
